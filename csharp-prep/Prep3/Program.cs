9ef5f6328e9b9f4dac9c97965a070f51adf9f4ad
viniciuserocca <vinicius.erocca@gmail.com>
2024-04-24 19:44:47 -0300

    prep3 exercise

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,43 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string play = "yes";
+
+        while (play == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+
+            Console.WriteLine("Guess a number between 1 and 100");
+
+            int guess = 0;
+            int count = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+                guess = int.Parse(userInput);
+                count ++;
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+            Console.WriteLine($"It took you {count} guesses");
+            Console.WriteLine("Do you wanna play it again? (yes/no) ");
+            play = Console.ReadLine();
+        }
+        Console.WriteLine("Thanks for playing");
     }
 }
