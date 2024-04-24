cd05513839b2ac8a42e4c107e81772bd9b9aac10
viniciuserocca <vinicius.erocca@gmail.com>
2024-04-24 19:00:37 -0300

    prep2 exercise

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your score: ");
+        string inputFromUser = Console.ReadLine();    
+
+        int score = int.Parse(inputFromUser);
+        string letter = "";
+        string sign = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else if (score < 60)
+        {
+            letter = "F";
+        }
+
+        int lastDigit = score % 10;
+        Console.WriteLine(lastDigit);
+
+        if (lastDigit >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+            sign = "";
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't make it. Good luck next time!");
+        }
+
     }
 }
