036fb13dab2cfae3d39b7edd1892c5632b5f863b
viniciuserocca <vinicius.erocca@gmail.com>
2024-05-09 03:04:12 -0300

    learning3 activity added

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Xml.XPath;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {   
+        string result = $"{_top}/{_bottom}";
+        return result;
+    }
+
+    public double GetDecimalValue()
+    {   
+        return (double)_top / (double)_bottom;
+    }
+
+    // Creating Getters and Setters 
+  public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
+    {   
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Console.WriteLine();
+        Console.WriteLine("Testing Setters and Getters: ");
+
+        Fraction f5 = new Fraction();
+        f5.SetTop(100);
+        f5.SetBottom(100);
+        Console.WriteLine(f5.GetTop());
+        Console.WriteLine(f5.GetBottom());
     }
 }
